a2ae0b68b458f13595f15afe597c799b6786d41e
Juan <koenigjuan@gmail.com>
2025-08-27 16:43:23 -0300

    testing

---
+++ testing.js
@@ -0,0 +1,19 @@
+// let popUpAlert = function() {
+//     alert("this works fine");
+// }
+
+// let converter = function(usd) {
+//     return usd*1200*1.21
+// }
+
+// //insert ARS 
+
+// console.log(converter(270));
+
+// let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// weekDays.pop();
+
+// let pushed = weekDays.push();
+// console.log(pushed);
+
