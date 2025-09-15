3475df1e18b07866b00e38396f1d20c82bc19ac4
Koenig Juan <koenigjuan@gmail.com>
2025-09-15 18:59:46 -0300

    apenas agrege 4 funciones y me distraje con otra cosa

--- entrega-1.js
+++ entrega-1.js
@@ -7,4 +7,14 @@ const convertirACelsius = temp => (temp - 32) / 1.8;
 
 const convertirAFahrenheit = temp => temp * 1.8 + 32;
 
-console.log(convertirAFahrenheit(100))
+const dolaresAPesos = dolares => dolares * 1466;
+
+const pesosADolares = pesos => pesos / 1466;
+
+const lbsAKG = lbs => lbs / 2.20462
+
+const kgALbs = kg => kg * 2.20462
+
+
+
+console.log(kgALbs(70))
