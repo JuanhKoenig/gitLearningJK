efcae2db0cb350c5f3e94c4171b6e78e50bc55a0
Koenig Juan <koenigjuan@gmail.com>
2025-09-17 13:31:05 -0300

    cambio testing por la entrega

--- entrega-1.js
+++ entrega-1.js
@@ -2,12 +2,20 @@
 // temperatura, monedas, unidades de fisica (velocidades, pesos, energias, etc)
 // usar swirch case en vez de if
 
+// -variables, const, arrays -listo
+// -script en el head con defer
+// -Minimo 3 funciones CON parametros
+// -Minimo un if
+// -Minimo un ciclo (for/while)
+// -TODO con prompt, console, alert
+// -NADA que exceda a la clase 4
+// -Se entrega SI O SI mediante github
+
 
 const convertirACelsius = temp => (temp - 32) / 1.8;
 
 const convertirAFahrenheit = temp => temp * 1.8 + 32;
 
-const dolaresAPesos = dolares => dolares * 1466;
 
 const pesosADolares = pesos => pesos / 1466;
 
@@ -15,6 +23,28 @@ const lbsAKG = lbs => lbs / 2.20462
 
 const kgALbs = kg => kg * 2.20462
 
+const dolaresAPesos = dolares => dolares * 1466;
 
 
 console.log(kgALbs(70))
+
+
+const menu = function() {
+    const usersChoice = parseInt
+    
+    
+    
+    
+    (prompt("sample text"))
+    switch (usersChoice) {
+        case 1:
+        const dolares = parseInt(prompt("cuantos dolares"))
+        alert(dolaresAPesos(dolares))
+        break;
+
+        default:
+            alert("testing")
+    }
+}
+
+menu()
