2fb6c635d98006c8f4fd893e2dd3ca7dc55191d6
Koenig Juan <koenigjuan@gmail.com>
2025-09-19 16:57:03 -0300

    agregue los if statements y cases 2 y 3

--- entrega-1.js
+++ entrega-1.js
@@ -5,12 +5,14 @@
 
 // -variables, const, arrays -listo
 // -script en el head con defer
-// -Minimo 3 funciones CON parametros
-// -Minimo un if
+// -Minimo 3 funciones CON parametros -listo
+// -Minimo un if -listo
 // -Minimo un ciclo (for/while)
-// -TODO con prompt, console, alert
-// -NADA que exceda a la clase 4
-// -Se entrega SI O SI mediante github
+// -TODO con prompt, console, alert -listo
+// -NADA que exceda a la clase 4 -listo
+// -Se entrega SI O SI mediante github -ok
+
+
 
 
 const convertirACelsius = temp => (temp - 32) / 1.8;
@@ -26,29 +28,53 @@ const lbsAKG = lbs => lbs / 2.20462
 const kgALbs = kg => kg * 2.20462
 
 
-
-console.log(kgALbs(70))
-
-
 const menu = function() {
-    const usersChoice = parseInt
-    
-    
-    
-    
-    (prompt("sample text"))
+    const usersChoice = parseInt(prompt("que desea hacer \n1.convertir monedas \n2.convertir pesos \n3.convertir temperaturas"))
+
+
     switch (usersChoice) {
         case 1:
-        const dolares = parseInt(prompt("cuantos dolares"))
-        alert(dolaresAPesos(dolares))
+        const userMoneyType = parseInt(prompt("eliga una opcion:\n1.pesos a dolares \n2.dolares a pesos"))
+        if (userMoneyType == 1) {
+            const userPesos = parseInt(prompt("escriba el monto en pesos:"))
+            alert(pesosADolares(userPesos))
+        }   else if (userMoneyType == 2) {
+            const userDolares = parseInt(prompt("escriba el monto en dolares:"))
+            alert(dolaresAPesos(userDolares))
+        }   else {
+            alert("opcion invalida") //redondear los numeros para reducir decimales
+        }
         break;
 
         case 2:
-        const pesos = parseInt(prompt("cuantos pesos"))
-        alert(pesosADolares(pesos))
+        const userWeightType = parseInt(prompt("que conversion desea hacer? \n1. Kgs a lbs\n.2 lbs a Kgs"))
+        
+        if (userWeightType == 1) {
+            const userWeight = parseFloat(prompt("ingrese el peso en Kgs"))
+            alert(kgALbs(userWeight))
+        }   else if (userWeightType == 2) {
+            const userWeight = parseFloat(prompt("ingrese el peso en lbs"))
+            alert(lbsAKG(userWeight))
+        }   else {
+            alert("opcion invalida")
+        }
         break;
+
+        case 3:
+            const userTempType = parseInt(prompt("que conversion desea hacer? \n1. °C a °F\n.2 °F a °C"))
+            if (userTempType == 1) {
+                const userTemp = parseFloat(prompt("ingrese la temperatura en celcius"))
+                alert(convertirAFahrenheit(userTemp))
+            }   else if (userTempType == 2) {
+                const userTemp = parseFloat(prompt("ingrese la temperatura en fahrenheit"))
+                alert(convertirACelsius(userTemp))
+            }   else {
+                alert("opcion invalida")
+            }
+
         default:
-            alert("testing")
+            alert("testing") 
+
     }
 }
 
